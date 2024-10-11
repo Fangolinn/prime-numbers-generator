3339a03d502be1b3a2d45bcf917a0b5ed8ac882d
Fangolin <jakubkowalczyk0192@o2.pl>
2024-10-11 20:57:21 +0200

    Add integration test validating if wrong 'limit' query datatype results in bad request status.

--- backend/app/src/test/java/prime_numbers/api/PrimeControllerTest.java
+++ backend/app/src/test/java/prime_numbers/api/PrimeControllerTest.java
@@ -1,6 +1,8 @@
 package prime_numbers.api;
 
 import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +36,16 @@ class PrimesControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/primes?limit=" + limit))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo(expectedResponse)));
+    }
+
+    /**
+     * Test if the API correctly returns a 400 Bad Request when the limit is an invalid type.
+     */
+    @Test
+    void getPrimesInvalidLimit() throws Exception {
+        String limit = "abc";
+
+        mvc.perform(MockMvcRequestBuilders.get("/primes?limit=" + limit))
+                .andExpect(status().isBadRequest());
     }
 }
